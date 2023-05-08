d4f9869958ec2424f04c5c29ae9dd6f5cc2c5de8
Zeckon101 <jordy.van.mol@gmail.com>
2023-05-08 18:42:34 +0200

    Voorbeeld tennis werkt

---
+++ Cursed Tennis/Assets/scripts/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using MLAgents;
+using Unity.MLAgents;
+
+public class test : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
